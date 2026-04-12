34af45021360aae736cdb07a43283b0d39ccd670
greg <guxuede@gmail.com>
2026-04-12 12:17:57 +0800

    use limbo as FSM, use LPC actor. make weapon and item changeable

--- scenes/compoments/HitBox.gd
+++ scenes/compoments/HitBox.gd
@@ -1,7 +1,1 @@
 class_name HitBox extends HitCompoment
-
-signal Damaged(damage: int,hitPostion:Vector2)
-
-func takeDamage(damage: int,  hitPostion:Vector2) ->void:
-	print("takeDamage:", damage)
-	Damaged.emit(damage,hitPostion)
